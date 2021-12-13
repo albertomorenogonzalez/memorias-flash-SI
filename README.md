642ded6ea86f23e4dc182611f3793cebf67e4d46
Alberto Moreno González <91873488+albertomorenogonzalez@users.noreply.github.com>
2021-12-13 23:56:59 +0100

    Añadidos todos los archivos de memorias-flash-SI

---
+++ README.md
@@ -0,0 +1,4 @@
+# memorias-flash-SI
+Página web sobre las memorias flash para la asignatura de Sistemas Informáticos
+
+Link de la página: https://albertomorenogonzalez.github.io/memorias-flash-SI/
---
+++ javascripts/javascript.js
@@ -0,0 +1,10 @@
+/********* script de la pagina web de memoiras flash *********/
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}
